c574bfa1cb6e1720554f6144ecdd46f5da037262
Artem Adeev <artemadeev42@gmail.com>
2023-10-28 13:04:58 +0300

    first task

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
+function generateArray(length, number) {
+    return [...new Array(length)].map(() => Math.round(Math.random() * number));
+}
+const array = generateArray(8, 10);
 
+console.log(array);
+
+// const arrayFilter = array.filter((element) => element % 2 === 0);
+// const sum1 = arrayFilter.reduce((accum, elem) => accum + Math.sqrt(elem), 0);
+// console.log('g' + sum1);
+const sum = () => array.reduce((accum, elem) => (elem % 2 === 0 ? accum + Math.sqrt(elem) : accum), 0);
+console.log(sum().toFixed(2));
